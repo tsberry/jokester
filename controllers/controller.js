fdd5ed0f219023e17e79009db39dfcd89b094ae9
nljacob <neehaljacob@gmail.com>
2018-04-26 12:51:44 -0700

    added routs

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,4 +6,20 @@ router.get("/", function (req, res) {
     res.render("index");
 });
 
+router.get("/joketopics", function (req, res) {
+    res.render("joketopics")
+});
+
+router.get("/login", function (req, res) {
+    res.render("login")
+});
+
+router.get("/search", function (req, res) {
+    res.render("search")
+});
+
+router.get("/signup", function (req, res) {
+    res.render("signup")
+});
+
 module.exports = router;
