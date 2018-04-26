7f3acd82fc496838fa9d19a5f465d81388e8e3c0
Yash Arya <yasharya11@gmail.com>
2018-04-26 11:46:23 -0700

    ass

---
+++ models/joke.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+    var comments_lookup_table = sequelize.define("comments_lookup_table", {
+        isUpvote: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true
+        }
+    });
+
+    comments_lookup_table.associate = function(models) {
+        comments_lookup_table.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+
+        comments_lookup_table.belongsTo(models.Comment, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+    return comments_lookup_table;
+};
