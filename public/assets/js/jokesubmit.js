cf26cfafa65a85b9f0805b17b1cdae0eeba2efbc
unknown <tberry6@gmail.com>
2018-04-27 14:27:14 -0700

    Joke submission works now

--- public/assets/js/jokesubmit.js
+++ public/assets/js/jokesubmit.js
@@ -1,38 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Getting references to our form and inputs
     var jokeForm = $("form.joke");
-    var jokeText = $("input#joketext");
-    var category = $("input#category");
-  
 
-    jokeForm.on("submit", function(event) {
-      event.preventDefault();
-      var userData = {
-        joketext: jokeText.val().trim(),
-        category: category.val().trim()
-      };
-  
-      if (!userData.joketext || !userData.category) {
-        return;
-      }
-  
-      submitJoke(userData.joketext, userData.category);
-      jokeText.val("");
-      category.val("");
+    jokeForm.on("submit", function (event) {
+        event.preventDefault();
+        var jokeText = $("input#joketext");
+        var category = $("select#category option:selected");
+        var userData = {
+            joketext: jokeText.val().trim(),
+            category: category.text().substring(0, category.text().length - 6)
+        };
+
+        console.log(userData);
+        if (!userData.joketext || !userData.category) {
+            return;
+        }
+
+        submitJoke(userData.joketext, userData.category);
+        jokeText.val("");
+        category.val("");
     });
-  
+
     // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
     function submitJoke(joketext, category) {
-      $.post("/api/jokes", {
-        joketext: joketext,
-        category: category
-      }).then(function(data) {
-        window.location.replace(data);
-        // If there's an error, log the error
-      }).catch(function(err) {
-        console.log(err);
-      });
+        $.post("/api/jokes", {
+            text: joketext,
+            category: category
+        }).then(function (data) {
+            window.location.replace(data);
+        });
     }
-  
-  });
-  
+
+});
